6d7f4710f10a3f7e34d0b05d951980ba7b9a21b9
Despotidis Pantelis <itp22109@hua.gr>
2023-09-04 22:00:17 +0300

    update external files from Control

---
+++ Vagrant_External/top-oc-srv-01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+device="oc-srv-01"
+
+output=$(ssh $device "top -b -n 1")
+
+cpu_percent=$(echo "$output" | awk '/%Cpu/ {print $2}')
+mem_total=$(echo "$output" | awk '/MiB Mem/ {print $4}')
+mem_used=$(echo "$output" | awk '/MiB Mem/ {print $8}')
+mem_percent=$(echo "scale=2; ($mem_used / $mem_total) * 100" | bc)
+pids=$(echo "$output" | awk '/Tasks:/{print $2}')
+
+echo -e "Device Name,CPU(%),Mem Total,Mem Used,Mem(%),PID's"
+echo -e "$device,$cpu_percent,$mem_total,$mem_used,$mem_percent,$pids"
---
+++ Vagrant_External/top-owncloud.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+container="owncloud_server"
+
+#docker Metric
+docker_stats=$(ssh control "docker stats --no-stream $container | awk 'NR>1{gsub(/%/, \"\", \$3); gsub(/MiB|GiB/, \"\", \$4); gsub(/MiB|GiB|/, \"\", \$6); gsub(/B|%/, \"\", \$7); gsub(/MB/, \"\", \$14); gsub(/\/,/, \",\"); print \$3 \",\" \$4 \",\" \$6 \",\" \$7 \",\" \$14}'")
+
+
+echo -e "Device/DockerStats,CPU%,MEM USED,MEM TOTAL,MEM%,PIDS"
+echo -e "$container,$docker_stats"
